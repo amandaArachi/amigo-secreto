c712dcfb7c034ea7740230277157301f538b75f5
amandaArachi <amanda.arachi@hotmail.com>
2025-07-17 17:19:16 -0300

    botão sortear

--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
+//validações: qtd min de participante, campo vazio(ok), valor repetido.
+let listaNomes = [];
+
 function adicionar() {
-    let nome = document.getElementById('nome-amigo').value;
+    let nome = document.getElementById('nome-amigo');
     let incluidos = document.getElementById('lista-amigos');
+    listaNomes.push(nome.value);
+
     if(incluidos.textContent == '') {
-        incluidos.textContent = nome;
+        incluidos.textContent = nome.value;
     } else {
-        incluidos.textContent = incluidos.textContent + ', ' + nome;
+        incluidos.textContent = incluidos.textContent + ', ' + nome.value;
+    }
+    nome.value = '';
+}
+
+function sortear() {
+    embaralhar(listaNomes);
+    let sorteio = document.getElementById('lista-sorteio');
+     
+
+    for(let i = 0; i < listaNomes.length; i++) {
+        if(i == listaNomes.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + listaNomes[i] + ' --> ' + listaNomes[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + listaNomes[i] + ' --> ' + listaNomes[i + 1] + '<br>';
+        }
+    }
+
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
